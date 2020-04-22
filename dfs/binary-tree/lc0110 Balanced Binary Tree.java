358d6f181661641c96949c3efeadd8e68eaee044
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-23 01:27:01 +1000

    Update lc0110 Balanced Binary Tree.java

--- dfs/binary-tree/lc0110 Balanced Binary Tree.java
+++ dfs/binary-tree/lc0110 Balanced Binary Tree.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// 1ms 50.36%
 class Solution {
     public boolean isBalanced(TreeNode root) {
         return dfs(root)[0] == 1;
@@ -23,5 +24,24 @@ class Solution {
         if(left[0] == 0 || right[0] == 0 || Math.abs(left[1] - right[1]) > 1) return failReturn;
         
         return new int[]{1, Math.max(left[1], right[1]) + 1};
+    }
+}
+
+// 0ms 100%
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != null;
+    }
+    
+    // return null for fail, int for valid
+    Integer dfs(TreeNode n){
+        // computer result for current level and terminate when there is no children
+        if(n == null) return 0;
+        
+        // compute result for current level by aggregating results from children
+        Integer left = dfs(n.left), right = dfs(n.right);
+        if(left == null || right == null || Math.abs(left - right) > 1) return null;
+        
+        return Math.max(left, right) + 1;
     }
 }
