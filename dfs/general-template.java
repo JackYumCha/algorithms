2e29efc5cef43760fbff02363167cc247bd594ab
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-23 00:52:36 +1000

    Update general-template.java

--- dfs/general-template.java
+++ dfs/general-template.java
@@ -3,9 +3,13 @@
 returnType dfs(Node n, parameters){
   Node[] next = [get connected children or neightbours];
   returnType result = [compute result for current node];
+  // optional early termination
+  if(condition(result)) return result;
   for(Node i : next){
     // aggregation could be sum, could be && and anything
     result = aggregate(dfs(i, child-parameters))
+    // optional early termination
+    if(condition(result)) return result;
   }
   return result;
 }
