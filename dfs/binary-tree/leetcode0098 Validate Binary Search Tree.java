eba948098e6842d88be0d7708ae5d7d5f98697d9
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-23 00:58:42 +1000

    Create leetcode0098 Validate Binary Search Tree.java

---
+++ dfs/binary-tree/leetcode0098 Validate Binary Search Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, null, null);
+    }
+    boolean dfs(TreeNode n, Integer from, Integer to){
+        // compute result for current level and early termination
+        if(n == null) return true;
+        
+        // compute result for current level and early termination
+        if((from != null && n.val <= from) || (to != null && n.val >= to)) return false;
+        
+        // aggregate result for child level and early termination
+        if(!dfs(n.left, from, n.val)) return false;
+        if(!dfs(n.right, n.val, to)) return false;
+        
+        // return result
+        return true;
+    }
+}
