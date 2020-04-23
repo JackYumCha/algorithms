3a9b246d81e956c00515a3ed20ca20d93ec4f881
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-23 10:19:50 +1000

    Create process-all-template.java

---
+++ dfs/process-all-template.java
@@ -0,0 +1,30 @@
+// in general
+aggregatedType overall_aggregation_value;
+
+returnType dfs(Node n, parameters){
+  Node[] next = [get connected children or neightbours];
+  returnType result = [compute result for current node];
+  // optional early termination
+  
+  // optional depend on traverse order
+  do_something(n);
+  
+  // optional aggregate global results
+  aggregate_overall(result, overall_aggregation_value);
+  
+  if(condition(result)) return result;
+  for(Node i : next){
+    // aggregation could be sum, could be && and anything
+    result = aggregate(dfs(i, child-parameters))
+    // optional early termination
+    if(condition(result)) return result;
+  }
+  // optional depend on traverse order
+  do_something(n);
+  
+  // optional aggregate global results
+  aggregate_overall(result, overall_aggregation_value);
+  
+  return result;
+}
+
