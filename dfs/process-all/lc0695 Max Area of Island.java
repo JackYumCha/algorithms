cb2c42002503e240778639330a815024bd366541
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-23 10:32:57 +1000

    Create lc0695 Max Area of Island.java

---
+++ dfs/process-all/lc0695 Max Area of Island.java
@@ -0,0 +1,26 @@
+// 2ms 99.34%
+class Solution {
+    
+    int cr, cc;
+    int[][] gd;
+    public int maxAreaOfIsland(int[][] grid) {
+        gd = grid;
+        cr = gd.length;
+        if(cr == 0) return 0;
+        cc = gd[0].length;
+        if(cc == 0) return 0;
+        int max = 0;
+        for(int r = 0; r < cr; r++){
+            for(int c = 0; c < cc; c++){
+                max = Math.max(max, dfs(r, c));
+            }
+        }
+        return max;
+    }
+    
+    int dfs(int r, int c){
+        if(r < 0 || r >= cr || c < 0 || c >= cc || gd[r][c] == 0) return 0;
+        gd[r][c] = 0;
+        return 1 + dfs(r, c+1) + dfs(r,c-1) + dfs(r+1,c) + dfs(r-1,c);
+    }
+}
