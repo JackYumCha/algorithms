7c9f1d421aec1045504c6c97bfc980c3ed4df1cb
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-23 10:35:22 +1000

    Update lc0695 Max Area of Island.java

--- dfs/process-all/lc0695 Max Area of Island.java
+++ dfs/process-all/lc0695 Max Area of Island.java
@@ -10,6 +10,8 @@ class Solution {
         cc = gd[0].length;
         if(cc == 0) return 0;
         int max = 0;
+        
+        // need to scan every location once
         for(int r = 0; r < cr; r++){
             for(int c = 0; c < cc; c++){
                 max = Math.max(max, dfs(r, c));
@@ -20,7 +22,9 @@ class Solution {
     
     int dfs(int r, int c){
         if(r < 0 || r >= cr || c < 0 || c >= cc || gd[r][c] == 0) return 0;
+        // trick: mutate the grid itself to avoid process more than once
         gd[r][c] = 0;
+        // aggregate the result, which is the sum of island value
         return 1 + dfs(r, c+1) + dfs(r,c-1) + dfs(r+1,c) + dfs(r-1,c);
     }
 }
