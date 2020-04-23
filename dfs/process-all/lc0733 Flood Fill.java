cd3c98cf95afcc1bf32bf5453691a437cf1d9f41
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-23 10:41:52 +1000

    Create lc0733 Flood Fill.java

---
+++ dfs/process-all/lc0733 Flood Fill.java
@@ -0,0 +1,36 @@
+class Solution {
+    // reference to the matrix
+    int[][] gd;
+    
+    // shared parameters for dfs
+    int cr, cc, color, replace;
+    
+    // avoid traverse more than once
+    boolean[][] visited;
+    
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        gd = image;
+        cr = gd.length;
+        if(cr == 0) return gd;
+        cc = gd[0].length;
+        if(cc == 0) return gd;
+        color = gd[sr][sc];
+        replace = newColor;
+        visited = new boolean[cr][cc];
+        dfs(sr, sc);
+        return gd;
+    }
+    
+    void dfs(int r, int c){
+        if(r < 0 || r >= cr || c < 0 || c >= cc || visited[r][c] || gd[r][c] != color) return;
+        gd[r][c] = replace;
+        visited[r][c] = true;
+        
+        // traverse neighbours
+        dfs(r,c+1);
+        dfs(r,c-1);
+        dfs(r+1,c);
+        dfs(r-1,c);
+    }
+    
+}
