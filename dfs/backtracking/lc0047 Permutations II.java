0267e56739a1fd78fe05efba7be70e63dee09d55
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-25 01:58:10 +1000

    Update lc0047 Permutations II.java

--- dfs/backtracking/lc0047 Permutations II.java
+++ dfs/backtracking/lc0047 Permutations II.java
@@ -1,44 +1,34 @@
 // 1ms 99.30%
 class Solution {
-    ArrayList<List<Integer>> arr;
-    int[] nums;
-    boolean[] used;
-    int[] r;
+    ArrayList<List<Integer>> result;
     int l;
+    int[] ns, arr;
+    boolean[] used;
     public List<List<Integer>> permuteUnique(int[] nums) {
-        this.nums = nums;
-        l = nums.length;
-        arr = new ArrayList();
-        Arrays.sort(nums);
-        r = new int[l];
+        ns = nums; // [1,2,3,2,1] -> [1,1,2,2,3]
+        l = ns.length; 
+        Arrays.sort(ns); // [1,1,2,2,3]
+        arr = new int[l];
         used = new boolean[l];
-        dfs(0, 0, -1);
-        return arr;
+        result = new ArrayList();
+        dfs(0, 0, null);
+        return result;
     }
-    void dfs(int i, int from, int last){
+    
+    void dfs(int i, int from, Integer previous){
         if(i == l){
-            ArrayList<Integer> a = new ArrayList(l);
-            for(int n : r) a.add(n);
-            arr.add(a);
+            ArrayList<Integer> a =new ArrayList();
+            for(int k: arr) a.add(k);
+            result.add(a);
         }
         else{
-            if(nums[i] == last){
-                for(int j = from; j < l; j++){
-                    if(used[j]) continue;
-                    used[j] = true;
-                    r[j] = nums[i];
-                    dfs(i+1, j+1, nums[i]);
-                    used[j] = false;
-                }
-            }
-            else{
-                for(int j = 0; j < l; j++){
-                    if(used[j]) continue;
-                    used[j] = true;
-                    r[j] = nums[i];
-                    dfs(i+1, j+1, nums[i]);
-                    used[j] = false;
-                }
+            for(int j = (previous == null || previous != ns[i]) ? 0 : from; j < l; j++){
+                if(used[j]) continue;
+                used[j] = true;
+                arr[j] = ns[i];
+                dfs(i + 1, j + 1, ns[i]);
+                // arr[j] = ;
+                used[j] = false;
             }
         }
     }
