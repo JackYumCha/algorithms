860811e22d761ad4002d73d186df494b0655035a
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-04-24 22:25:19 +1000

    Update lc0046 Permutations.java

--- dfs/backtracking/lc0046 Permutations.java
+++ dfs/backtracking/lc0046 Permutations.java
@@ -25,10 +25,41 @@ class Solution {
             for(int j = 0; j < l; j++){
                 if(used[j]) continue;
                 used[j] = true;
-                idx[i] = j;
+                idx[i] = j; // idx[j] = i; both will work
                 dfs(i+1);
                 used[j] = false;
             }
         }
+    }
+}
+
+// swap - permute
+class Solution {
+    ArrayList<List<Integer>> arr;
+    int[] nums;
+    public List<List<Integer>> permute(int[] nums) {
+        this.nums = nums;
+        arr = new ArrayList();
+        dfs(0, nums.length - 1);
+        return arr;
+    }
+    void dfs(int from, int to){
+        if(from == to){
+            ArrayList<Integer> a = new ArrayList();
+            for(int n: nums) a.add(n);
+            arr.add(a);
+        }
+        else{
+            for(int i = from; i <= to; i++){
+                swap(from, i);
+                dfs(from + 1, to);
+                swap(from, i);
+            }
+        }
+    }
+    void swap(int a, int b){
+        int c = nums[b];
+        nums[b] = nums[a];
+        nums[a] = c;
     }
 }
