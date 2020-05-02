66e32d55b766d44276744ccb1928c84621eba92e
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-05-03 00:52:16 +1000

    Update lc0772 Basic Calculator III (DFS+D&C).java

--- dfs/text-calculator/lc0772 Basic Calculator III (DFS+D&C).java
+++ dfs/text-calculator/lc0772 Basic Calculator III (DFS+D&C).java
@@ -1,5 +1,5 @@
 // 1ms 100%
-public class Solution {
+class Solution{
     String s;
     int i, l;
     public int calculate(String s) {
@@ -8,59 +8,57 @@ public class Solution {
         l = s.length();
         return dfs1();
     }
-    int dfs1(){ // compute an expression without () compute add/deduct only
-        int sum = 0, sign = 1;
+    int dfs1(){ // () and +-
+        int sum = 0, sign = 1; 
         while(i < l){
             char c = s.charAt(i);
-            if(c == '(' || (c >= '0' && c <= '9') ){
-                int n = dfs2();
-                sum += sign * n;
+            if((c>='0' && c<='9') || c == '(' ){
+                sum += sign * dfs2();
                 sign = 1;
             }
-            else if(c == ')'){
-                return sum;
-            }
             else if(c == '-'){
-                sign *= -1;
+                sign = -sign;
+            }
+            else if(c==')'){
+                return sum;
             }
             i++;
         }
         return sum;
     }
-    int dfs2(){ // read a multiply/divide group
-        int p = 1;
-        boolean mul = true;
-        while(i < l){
+    int dfs2(){ //  
+        int product = 1; // product
+        boolean isMulti = true; // is multiply(true) or divide(false)
+        while(i<l){
             char c = s.charAt(i);
-            if(c >= '0' && c <= '9'){
+            if(c>='0' && c<='9'){
                 int n = dfs3();
-                if(mul) p *= n;
-                else p /= n;
+                if(isMulti) product *= n;
+                else product /= n;
             }
             else if(c == '*'){
-                mul = true;
+                isMulti = true;
             }
             else if(c == '/'){
-                mul = false;
+                isMulti = false;
             }
             else if(c == '('){
                 i++;
                 int n = dfs1();
-                if(mul) p *= n;
-                else p /= n;
+                if(isMulti) product *= n;
+                else product /= n;
             }
-            else if(c == ')' || c == '+' || c == '-'){
+            else if(c==')' || c=='+' || c=='-'){
                 i--;
-                return p;
+                return product;
             }
             i++;
         }
-        i--;
-        return p;
+        return product;
     }
-    int dfs3(){ // read a number
+    int dfs3(){ // read a num
         int n = 0;
-        while(i < l && s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+        while(i<l && s.charAt(i) >= '0' && s.charAt(i) <= '9'){
             n = n * 10 + (s.charAt(i) - '0');
             i++;
         }
