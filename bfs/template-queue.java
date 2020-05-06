b9f0fae29b43b059f754f2a799143e415e7c8e55
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-05-06 22:20:10 +1000

    Update template-queue.java

--- bfs/template-queue.java
+++ bfs/template-queue.java
@@ -1,1 +1,11 @@
-
+void method(){
+    LinkedList<Type> q = new LinkedList();
+    while(!q.isEmpty()){
+      Type n = q.pollFirst();
+      // check if n meets your condition, break and terminate if necessary
+      for(Type child : getChildren(n)){
+          // check if child meet the condition
+          q.offerLast(child);
+      }
+    }
+}
