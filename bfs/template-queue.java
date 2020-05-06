fdd52845a3000f4c847fed24b5152e145bc2ab4d
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-05-06 22:28:22 +1000

    Update template-queue.java

--- bfs/template-queue.java
+++ bfs/template-queue.java
@@ -1,5 +1,9 @@
 void method(){
-    LinkedList<Type> q = new LinkedList();
+    LinkedList<Type> q = new LinkedList(); // could be WrapType if needed
+    
+    // dijkstra's if you need to mark something has been accessed. can be boolean[] for arry boolean[][] for matrix
+    HashSet<Type> visited = new HashSet(); 
+    
     while(!q.isEmpty()){
       Type n = q.pollFirst();
       // check if n meets your condition, break and terminate if necessary
@@ -8,4 +12,9 @@ void method(){
           q.offerLast(child);
       }
     }
+}
+
+class WrapType{
+    Type item;
+    int step; // if you need to count steps or find min cost etc.
 }
