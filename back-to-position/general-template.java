28ea93c54680d0e255297e7a2e3f38c703b50c72
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-05-26 20:50:10 +1000

    Create general-template.java

---
+++ back-to-position/general-template.java
@@ -0,0 +1,24 @@
+class Solution {
+    int[] ns;
+    int n;
+    public int findDuplicate(int[] nums) {
+        ns = nums;
+        n = ns.length - 1;
+        for(int i = 0; i < n; i++){
+            find(i, true);
+        }
+        return 1;
+    }
+    int find(int index, boolean start){
+        int j = ns[index];
+        // logic depends on the problem
+        ns[index] = start ? -1 : index; // or set to something as a place holder
+        if(j == index){
+            ns[index] = j;
+            return ???
+        }
+        else{
+            return find(j, false);
+        }
+    }
+}
