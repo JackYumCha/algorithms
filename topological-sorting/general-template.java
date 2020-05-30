012f89d4b833eeb63ade1b42d325f4969fcb6c35
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-05-30 21:25:29 +1000

    Create general-template.java

---
+++ topological-sorting/general-template.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean /* List<Integer> if return order of sorting */ topologicalSorting(int n, int[][] edges) {
+        int[] dependencies = new int[n]; // dependencies, record how many prerequisite dependencies a node has
+        ArrayList<Integer>[] toNodes = new ArrayList[n]; // record the edges from i to other nodes
+        for(int i = 0; i < n; i++){
+            toNodes[i] = new ArrayList();
+        }
+        for(int[] pr : prs){
+            dependencies[pr[1]]++;
+            toNodes[pr[0]].add(pr[1]);
+        }
+        LinkedList<Integer> q = new LinkedList();
+        int count = 0;
+        ArrayList<Integer> orders = new ArrayList();
+        for(int i = 0; i < n; i++){
+            if (dependencies[i] == 0){
+                q.add(i);
+                count++;
+            }
+        }
+        while(!q.isEmpty()){
+            int k = q.pollFirst();
+            orders.add(k);
+            for(int d : toNodes[k]){
+                dependencies[d]--;
+                if(dependencies[d] == 0){
+                    q.add(d);
+                    count++;
+                }
+            }
+        }
+        return count == n; // true when there is a solution
+        return orders; // return the orders of a topological sorting solution
+    }
+}
